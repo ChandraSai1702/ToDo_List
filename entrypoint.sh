a8412aaca394204bdd27893cd1c7ee4c64a69a44
Sigmoid <sigmoid@sigmoid-ThinkPad-T460>
2024-10-04 18:02:24 +0530

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Wait for the database to be ready (useful for databases like PostgreSQL or MySQL)
-# Replace 'db' with your database hostname and '5432' with your database port
-echo "Waiting for database to be ready..."
-while ! nc -z db 5432; do
-  sleep 1
-done
-
 # Apply database migrations
 echo "Running database migrations..."
 python manage.py migrate --noinput
